78bfe23c32ae19c030946e7bf6d01cfd388a6260
Aryan <aryaninmail@gmail.com>
2024-03-29 17:36:04 +0530

    next-auth setup

---
+++ src/app/header.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { signIn, signOut, useSession } from "next-auth/react";
+import React from "react";
+
+const Header = () => {
+  const session = useSession();
+  console.log(session);
+
+  return (
+    <header>
+      {session.data ? (
+        <Button onClick={() => signOut()}>Sign out</Button>
+      ) : (
+        <Button onClick={() => signIn()}>Sign in</Button>
+      )}
+    </header>
+  );
+};
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ModeToggle } from "@/components/mode-toggle";
+import Provider from "./provider";
+import Header from "./header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,17 +21,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <div>
-            <ModeToggle />
-          </div>
-          {children}
-        </ThemeProvider>
+        <Provider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <div>
+              <ModeToggle />
+            </div>
+            <Header />
+            {children}
+          </ThemeProvider>
+        </Provider>
       </body>
     </html>
   );
